e53014cba7abb7ec5d5e81f621c197df38bccf46
R. Mutt <rrmutt@gmail.com>
2017-03-16 16:51:31 -0700

    fixed snooze script

--- localfoo/dash-daemon.py
+++ localfoo/dash-daemon.py
@@ -8,7 +8,9 @@ def arp_display(pkt):
       #print "found arp 1 hwsrc " + pkt[ARP].hwsrc + " psrc " + pkt[ARP].psrc 
       if pkt[ARP].hwsrc == '74:75:48:26:b2:ac': # ARP Prob
         sys.stdout.write('Dash ac detected\n')
-        Popen(['/usr/bin/python3','/home/pi/gith/rpi-utils/localfoo/snooze.py'])
+        Popen(['/usr/bin/python3',
+               '/home/pi/gith/rpi-utils/localfoo/snooze.py',
+               '15'])
         call(['date'])
       elif pkt[ARP].hwsrc == '74:c2:46:08:7c:d5':
         sys.stdout.write('print "Dash d5 detected\n')
--- localfoo/snooze.py
+++ localfoo/snooze.py
@@ -29,13 +29,17 @@ def touchfile(fn):
 
 if __name__ == "__main__":
 
-    snoozes = 3600
+    if len(sys.argv) < 2:
+        print("usage: snooze <n>  where n is minutes before mute")
+        exit(0)
+
+    snooze_time = 60*float(sys.argv[1])
     statusf = '/home/pi/.snooze'
     client = init_mpd()
 
-    # first, are we already aiting to snooze?
+    # first, are we already waiting to snooze?
     if os.path.isfile(statusf):
-        sys.stdout.write('already snoozin, stopped')
+        sys.stdout.write('already snoozin, stopped\n')
         #stop snooze and delete snoozefile
         client.stop()
         rmfile(statusf)
@@ -43,9 +47,19 @@ if __name__ == "__main__":
         # are we playing or streaming? 
         result = client.status();
         if result['state'] == 'play':
-            sys.stdout.write('snooze.py: snoozing in {}\n'.format(snoozes))
+            # client tends to timeout during sleep, so stop it and reopen
+            client.close()
+            client.disconnect()
+            then = time.time() + snooze_time
+            sys.stdout.write('snooze.py: snoozing until {0:f}\n'.format(
+                then))
+            sys.stdout.flush()
             touchfile(statusf)
-            time.sleep(snoozes)
+            while time.time() < then:
+                sys.stdout.write('now at {0:f}\n'.format(time.time() - then))
+                sys.stdout.flush()
+                time.sleep(30)
+            client = init_mpd()
             client.stop()
             rmfile(statusf)
             sys.stdout.write('snooze.py: snoozed\n')
